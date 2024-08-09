766fe2369cb399780653e8d0cbcd4ea555ed0784
Fons Elodie <elodie.fons66@gmail.com>
2024-08-09 09:01:17 +0200

    Commentaires ajoutés

--- Front/src/pages/PageErreur.jsx
+++ Front/src/pages/PageErreur.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom"
 
 function Error() {
     return (
-        <div className="error">
-            <h1>404</h1>
-            <h2>Oups! La page que vous demandez n&apos;existe pas.</h2>
-            <p>
-                <Link to="/">Retourner sur la page d&apos;accueil</Link>
-            </p>
+        <div className="mainContainer">
+            <div className="error">
+                <h1>404</h1>
+                <h2>Oups! La page que vous demandez n&apos;existe pas.</h2>
+                <p>
+                    <Link to="/">Retourner sur la page d&apos;accueil</Link>
+                </p>
+            </div>
         </div>
     )
 }
